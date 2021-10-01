74766eff7efd800d349769a51ec78171da84fedb
vadim_davydchenko <69358939+vadim-davydchenko@users.noreply.github.com>
2021-10-01 22:49:29 +0300

    Update varialbes.tf

--- 12/varialbes.tf
+++ 12/varialbes.tf
@@ -7,15 +7,27 @@ variable "region" {
 variable "instance_type" {
     description = "Enter Instance Type"
     type        = string
-    default     = t2.micro
+    default     = "t2.micro"
 }
 
 variable "allow_ports" {
     description = "List of Ports to open for server"
+    type = list
     default     = ["80", "443", "22", "8080"]
 }
 
 variable "enable_detailed_monitoring" {
     type = bool
     default = false
+}
+
+variable "common-tags" {
+  type = map
+  description = "Common tags to apply to all resources"
+  default = {
+    Owner      = "Vadim"
+    Project    = "Lead"
+    CostCenter = "12345"
+    Environment = "development"
+  }
 }
