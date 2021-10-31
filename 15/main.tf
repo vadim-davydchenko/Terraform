f6fc034008ef7d7fd6e99a8d79d232a5f7838f83
vadim_davydchenko <69358939+vadim-davydchenko@users.noreply.github.com>
2021-11-01 00:46:47 +0300

    Add files via upload

---
+++ 15/main.tf
@@ -0,0 +1,45 @@
+provider "aws" {
+  region = "ca-central-1"
+}
+variable "name" {
+  default = "vasya"
+}
+resource "random_string" "rds_password" {
+  length  = 12
+  special = true
+  override_special = "!#$&"
+  keepers = {
+    keeper1 = var.name
+  }
+}
+
+resource "aws_ssm_parameter" "rds_password" {
+  name = "/prod/mysql"
+  description = "Master Password for RDS MySQL"
+  type = "SecureString"
+  value = random_string.rds_password.result
+}
+
+data "aws_ssm_parameter" "my_rds_password" {
+  name = "/prod/mysql"
+  depends_on = [aws_ssm_parameter.rds_password]
+}
+
+resource "aws_db_instance" "default" {
+  identifier           = "prod-rds"
+  allocated_storage    = 10
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "prod"
+  username             = "administrator"
+  password             = data.aws_ssm_parameter.my_rds_password.value
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+  apply_immediately    = true
+}
+
+output "rds_password" {
+  value = "data.aws_ssm_parameter.my_rds_password.value"
+  sensitive = false
+}
